ba5494d097d719cc08580a3d1fbfdd9b051c4142
Vasco Matos <a85234@ualg.pt>
2023-12-21 17:48:54 +0000

    mudei o nome da side bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Ajuda',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
