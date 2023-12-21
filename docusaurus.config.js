e74f100f205e8cd5afaaef9df67ab6ba933a6d9d
Vasco Matos <a85234@ualg.pt>
2023-12-21 18:23:39 +0000

    alterei o titulo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,10 +65,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'oficina Faro',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'oficina Faro logo',
+          src: 'img/logoa.svg',
         },
         items: [
           {
